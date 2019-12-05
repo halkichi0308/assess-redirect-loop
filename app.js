e2e41fe5f8cf5e99976d5b8aeb38fd623e1f0172
halkichi0308 <m.kouda0308@gmail.com>
2019-12-05 19:32:14 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+const access_limit = 30
+app.locals.access_count = 0
+
+app.get('/redirect', (req, res) => {
+  if(app.locals.access_count < access_limit){
+    res.redirect(req.path); // reffer to HTTP request Header.
+    console.log('client ip:' + req.connection.remoteAddress)
+    app.locals.access_count++
+  }else{
+    console.log('Access limit reached.')
+    res.send(200, 'Sorry, Access limit reached.')
+    app.locals.access_count = 0
+  }
+})
+
+app.listen(port)
+
+console.log(`-Server listening at ${port} port -`)
+console.log('GET /redirect is .available.')
+
